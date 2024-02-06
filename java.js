95ec0f0bd1a8b8f97cf2ab8fd1e7313e100de202
maxime <maxime.quartier@supdevinci-edu.fr>
2024-02-06 15:11:09 +0100

    film avec image

--- java.js
+++ java.js
@@ -30,8 +30,20 @@
           if (response.results.length > 0) {
             response.results.forEach(movie => {
               const movieTitle = document.createElement('p');
+              const movieDate = document.createElement('p');
+              const imageBaseUrl = 'https://image.tmdb.org/t/p/';
+              const imageSize = 'w500'; // Taille de l'image
+              const imageUrl = imageBaseUrl + imageSize + movie.poster_path;
+              const movieElement = document.createElement('div');
+              movieElement.innerHTML = `
+                        <h2>${movie.title}</h2>
+                        <img src="${imageUrl}" alt="${movie.title}">
+                    `;
               movieTitle.textContent = movie.title;
+              movieDate.textContent  = movie.release_date;
+              resultsContainer.appendChild(movieElement);
               resultsContainer.appendChild(movieTitle);
+              resultsContainer.appendChild(movieDate);
             });
           } else {
             resultsContainer.textContent = 'Aucun résultat trouvé.';
@@ -43,4 +55,5 @@
         console.error('Erreur lors de la recherche de films:', error);
       }
     }
-  
+    const moviesContainer = document.getElementById('movies');
+
